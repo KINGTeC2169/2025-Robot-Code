38584f1614db3090399631b4ed12f7501932ba9f
Khanh <khanhngongnguyen2008@gmail.com>
2025-01-30 17:33:45 -0600

    Adjusted vision constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,8 +18,9 @@ public final class Constants {
   }
 
   public static final class Vision{
-    public static final double bargeTagHeight = 68.93; // Inches
-    public static final double LLHeight = -1; // Inches
-    public static final double LLAngle = -1; // Degrees
+    //public static final double bargeTagHeight = 68.93; // Inches
+    public static final double bargeTagHeight = 54;
+    public static final double LLHeight = 36; // Inches
+    public static final double LLAngle = 0; // Degrees
   }
 }
--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,15 +1,23 @@
 package frc.robot.subsystems;
 
+import java.security.Key;
+
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.Vision;
 
 public class Limelight extends SubsystemBase{
     private static NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    private ShuffleboardTab tab = Shuffleboard.getTab("Limelight");
 
     public Limelight(){
-        
+        tab.addNumber("Distance: ", () -> distanceFromTag());
+        tab.addNumber("Tx: ", () -> getTx());
+        tab.addNumber("Ty: ", () -> getTy());
+        tab.addNumber("Ta: ", () -> getTa());
     }
 
     public static double getTx(){
@@ -20,13 +28,17 @@ public class Limelight extends SubsystemBase{
         return table.getEntry("ty").getDouble(0);
     }
 
+    public static double getTa(){
+        return table.getEntry("ta").getDouble(0);
+    }
+
     public static boolean getTv(){
         return table.getEntry("tv").getDouble(0) > 0;
     }
 
     public static double distanceFromTag(){
         // Angle between floor and april tag in radians
-        double angle = (Vision.LLAngle + getTy()) * (Math.PI / 180);
+        double angle = Math.toRadians(Vision.LLAngle + getTy());
         // Difference between the height of barge tag and limelight
         double heightDiff = Vision.bargeTagHeight - Vision.LLHeight;
         return heightDiff / Math.tan(angle);
