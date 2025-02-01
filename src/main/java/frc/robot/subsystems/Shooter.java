cf86bb6dd108b63eeee7afc30292236284787c76
Ethan Mathew <ethmatt09@gmail.com>
2025-01-31 20:45:44 -0600

    continued working on shooter cad 😊

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,5 +1,58 @@
 package frc.robot.subsystems;
 
-public class Shooter {
-    
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Shooter extends SubsystemBase {
+
+    private final VelocityVoltage motorVelocity = new VelocityVoltage(0);
+
+    private TalonFX kraken; 
+
+    private double targetRPM = 0;
+
+    public Shooter() {
+        
+        kraken = new TalonFX(Constants.Ports.kraken);
+
+        var configs = new Slot0Configs();
+        configs.kP = 0.25;
+
+        kraken.getConfigurator().apply(configs, 0.05);
+    }
+
+    public void setPower(double power) {
+        kraken.set(power);
+    }
+
+    public double getCurrent(){
+        return kraken.getSupplyCurrent().getValueAsDouble();
+    }
+
+    public double getVoltage(){
+        return kraken.getSupplyVoltage().getValueAsDouble();
+    }
+
+    public double getRPM(){
+        return 60 * kraken.getRotorVelocity().getValueAsDouble();
+    }
+
+    public void setRPM(double rpm){
+        targetRPM = rpm;
+        motorVelocity.withVelocity(targetRPM);
+        kraken.setControl(motorVelocity);
+    }
+
+    public void stopShooter(){
+        kraken.set(0);
+    }
+
+
+
+
 }
