2beef0a7aa850dd6d2a19ab3caff40a7c75f9cdf
Bryson24 <bisonmannnn@gmail.com>
2025-02-06 15:41:43 -0600

    tried to add inverted function

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -23,8 +24,8 @@ public class Shooter extends SubsystemBase {
         
         kraken = new TalonFX(Constants.Ports.kraken);
 
-        var configs = new Slot0Configs();
-        configs.kP = 0.25;
+        var configs = new TalonFXConfiguration();
+        configs.Slot0.kP = 0.25;
 
         kraken.getConfigurator().apply(configs, 0.05);
     }
@@ -47,9 +48,6 @@ public class Shooter extends SubsystemBase {
 
     public void setRPM(double rpm){
         targetRPM = rpm;
-        //double testVelocity = (Math.PI * .00785 * targetRPM) / 60;
-        //motorVelocity.withVelocity(testVelocity);
-        //motorVelocity.withVelocity((2 * Math.PI) / targetRPM);
         testSpeed = targetRPM * (Math.PI * 0.00785);
         System.out.println(testSpeed);
         motorVelocity.withVelocity(-testSpeed);
