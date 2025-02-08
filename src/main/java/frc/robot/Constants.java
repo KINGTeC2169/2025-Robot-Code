0e315a5c8b51cb63ee065a108c2e4da7034bbbe5
Ethan Mathew <ethmatt09@gmail.com>
2025-02-07 19:17:45 -0600

    Added Pigeon

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,8 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+
+  public static class Ports{
+    public static final int pigeon = 2169;
+  }
 }
