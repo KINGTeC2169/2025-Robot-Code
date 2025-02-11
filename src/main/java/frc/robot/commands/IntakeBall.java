8ae1614299e8dd1a0932ccc2d680e51ff596e3cb
Khanh <khanhngongnguyen2008@gmail.com>
2025-02-11 16:42:51 -0600

    intake ball command

---
+++ src/main/java/frc/robot/commands/IntakeBall.java
@@ -0,0 +1,30 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake;
+
+public class IntakeBall extends Command{
+    private Intake intake;
+
+    IntakeBall(Intake i){
+        intake = i;
+        addRequirements(intake);
+    }
+
+    public void execute(){
+        // suck
+        intake.Sucker();
+    }
+
+    public void end(){
+        // no more suck
+        intake.stopTake();
+    }
+
+    public boolean isFinished(){
+        // idk how to do this *shrugging emoji*
+        // is ball in intake? is it not in intake? 
+        // who knows. not me. perhaps you? please know.
+        return false;
+    }
+}
