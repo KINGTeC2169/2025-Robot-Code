abc49fae7d130249732c4eec434481365d35d558
Khanh <khanhngongnguyen2008@gmail.com>
2025-02-11 17:05:15 -0600

    updated IntakeBall command

--- src/main/java/frc/robot/commands/IntakeBall.java
+++ src/main/java/frc/robot/commands/IntakeBall.java
@@ -5,26 +5,38 @@ import frc.robot.subsystems.Intake;
 
 public class IntakeBall extends Command{
     private Intake intake;
+    private boolean started;
 
-    IntakeBall(Intake i){
-        intake = i;
+    IntakeBall(Intake intake){
+        this.intake = intake;
         addRequirements(intake);
     }
 
+    @Override
     public void execute(){
         // suck
         intake.sucker();
+        // when motor get to certain speed its start time
+        if(intake.getRPM() > 1750){
+            started = true;
+        } else if (intake.getRPM() < 1600 && started){
+            // ???? what am i supposed to put here???
+            // i dont even know my own code bruh
+            isFinished();
+        }
     }
 
-    public void end(){
+    @Override
+    public void end(boolean interrupted){
         // no more suck
         intake.stopTake();
     }
 
+    @Override
     public boolean isFinished(){
         // idk how to do this *shrugging emoji*
         // is ball in intake? is it not in intake? 
         // who knows. not me. perhaps you? please know.
-        return false;
+        return true;
     }
 }
