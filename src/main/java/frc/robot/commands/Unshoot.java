f6198cada29f56ed08f9bf6ffcb3e04a263253cd
Bryson24 <bisonmannnn@gmail.com>
2025-02-25 16:30:52 -0600

    reverse shooter

---
+++ src/main/java/frc/robot/commands/Unshoot.java
@@ -0,0 +1,43 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Shooter;
+
+public class Unshoot extends Command {
+    
+    private Shooter shooter;
+    
+    public Unshoot(Shooter shoot){//, CommandXboxController controller) {
+        shooter = shoot;
+        addRequirements(shooter);
+    }
+    
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        shooter.setRPM(-1500); //* controller.getLeftTriggerAxis());
+    }
+    
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        shooter.setRPM(-1500);
+    }
+    
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        //Timer.delay(0.75);
+        //shooter.setPower(0);    
+    }
+    
+    
+    @Override
+    public boolean isFinished() {
+        return shooter.getRPM() < -1300;
+    }
+    
+
+}
+
